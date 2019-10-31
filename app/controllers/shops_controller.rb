278b90d67a04df72a1b549d56a8394f1c4ade8c4
c-04 <papukyulitos@gmail.com>
2019-11-01 04:38:59 +0900

    Update application

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -21,7 +21,8 @@ class ShopsController < ApplicationController
 
   def search
     @q = Shop.ransack(params[:q])
-    @shops = @q.result(distinct: true).order("RANDOM()").all
+    @shops = @q.result(distinct: true)
+    # .order("RANDOM()").all
   end
 
   def eat
